632090e0705a11b34b848f3eae84d1c0b71d44c6
Mussina <didi800702@gmail.com>
2017-07-10 00:41:43 +0800

    read daily net buy

---
+++ excelReader/JPNetBuy.cs
@@ -0,0 +1,43 @@
+﻿using NPOI.SS.UserModel;
+using NPOI.XSSF.UserModel;
+using NPOI;
+using NPOI.HSSF.UserModel;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockAnalysis.excelReader
+{
+    class DailyNetBuyIndex
+    {
+      //  public const int 
+    }
+   public class JPNetBuy
+    {
+        // net buy excel
+        private IWorkbook workbook;
+
+        private JPNetBuyData jpNetBuyData;
+
+        public JPNetBuy(String dirNetBuy)
+        {
+            jpNetBuyData = new JPNetBuyData();
+            using (FileStream file = new FileStream(dirNetBuy, FileMode.Open, FileAccess.Read))
+            {
+                workbook = new XSSFWorkbook(dirNetBuy);
+            }
+            readData();
+        }
+
+        private void readData()
+        {
+            ISheet sheet = workbook.GetSheetAt(0);
+            Double a = sheet.GetRow(2).GetCell(3).NumericCellValue;
+        }
+
+
+    }
+}
--- excelReader/JPNetBuyData.cs
+++ excelReader/JPNetBuyData.cs
@@ -11,8 +11,34 @@ namespace StockAnalysis.excelReader
     /// </summary>
     class JPNetBuyData
     {
+        public const int rowDiffNetBuy = 6;
+        public const int colDiffNetBuy = 3;
+        public Dealer dealer;
+        public InvestmentTrust investmentTrust;
+        public ForeignInvestor foreignInvestor;
+    }
+
+    class JuristicPersonData
+    {
         public long buyIn;
         public long sellOut;
         public long orderDiff;
     }
+    class Dealer : JuristicPersonData
+    {
+        public const int rowDiffNetBuy = 2;
+        public const int colDiffNetBuy = 3;
+    }
+    class InvestmentTrust : JuristicPersonData
+    {
+        public const int rowDiffNetBuy = 4;
+        public const int colDiffNetBuy = 3;
+    }
+    class ForeignInvestor : JuristicPersonData
+    {
+        public const int rowDiffNetBuy = 5;
+        public const int colDiffNetBuy = 3;
+    }
+   
+    
 }
