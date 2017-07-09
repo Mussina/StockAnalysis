ea20a84e0efce6bf36e11b50ee6c65550edd720b
Mussina <didi800702@gmail.com>
2017-07-09 23:16:57 +0800

    Initial

---
+++ excelReader/JPNetBuyData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockAnalysis.excelReader
+{
+    /// <summary>
+    /// JuristicPersonNetBuy's data
+    /// </summary>
+    class JPNetBuyData
+    {
+        public long buyIn;
+        public long sellOut;
+        public long orderDiff;
+    }
+}
