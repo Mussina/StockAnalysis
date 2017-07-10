f69baf7aede2c3a9cedd60ca2a1237dd676dfa28
Mussina <didi800702@gmail.com>
2017-07-10 23:56:27 +0800

    move test project to same folder

---
+++ TestStockAnalysis/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StockAnalysis.excelReader;
+
+namespace TestStockAnalysis
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            JPNetBuy jpNetBuy = new JPNetBuy("D:\\Coding4Fun\\3MainJuristicPersonRecord\\20170707.xlsx");
+
+        }
+    }
+}
